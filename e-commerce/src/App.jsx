868951b12655a59964208d33f7554a7b2d88c93c
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2024-09-22 14:55:27 +0700

    commit

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+// pages
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Card from "./pages/Card";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+// components
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw md:px-[7vw] lg:px-[9vw]">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
