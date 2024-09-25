0fb057b1d8e31a4a0280035600d9df7420e166d5
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2024-09-25 14:37:48 +0700

    collection page sort price function

--- e-commerce/src/pages/Collection.jsx
+++ e-commerce/src/pages/Collection.jsx
@@ -10,6 +10,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relavent');
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -36,16 +37,41 @@ const Collection = () => {
       );
     }
 
+    if (subCategory.length > 0) {
+      productCopy = productCopy.filter((item) =>
+        subCategory.includes(item.subCategory)
+      );
+    }
+
     setFilterProducts(productCopy);
   };
 
-  useEffect(() => {
-    setFilterProducts(products);
-  }, []);
+  const sortProducts = () => {
+    let ftCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(ftCopy.sort((a,b) => (a.price - b.price)));
+        break;
+      
+      case 'high-low':
+        setFilterProducts(ftCopy.sort((a, b) => (b.price - a.price)));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  }
+
 
   useEffect(() => {
     applyFilter();
   }, [category, subCategory]);
+
+  useEffect(() => {
+    sortProducts();
+  }, [sortType]);
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -148,8 +174,8 @@ const Collection = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title Text1={"ALL"} Text2={"COLLECTIONS"} />
           {/* Product Sort */}
-          <select className="border-2 border-gray-300 text-sm px-2">
-            <option value="relavent">Sort by: Relavant</option>
+          <select onChange={(e) => setSortType(e.target.value)} className="border-2 border-gray-300 text-sm px-2">
+            <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
           </select>
