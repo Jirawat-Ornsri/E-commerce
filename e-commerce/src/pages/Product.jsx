74e6f878e97964626a2a6ad097f42c05d48c28f7
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2024-09-26 16:02:50 +0700

    related product component

--- e-commerce/src/pages/Product.jsx
+++ e-commerce/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/shopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -51,35 +52,73 @@ const Product = () => {
         <div className="flex-1">
           <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
           <div className="flex items-center gap-1 mt-2">
-              <img src={assets.star_icon} alt="" className="w-3" />
-              <img src={assets.star_icon} alt="" className="w-3" />
-              <img src={assets.star_icon} alt="" className="w-3" />
-              <img src={assets.star_icon} alt="" className="w-3" />
-              <img src={assets.star_dull_icon} alt="" className="w-3" />
-              <p className="pl-2">(122)</p>
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_dull_icon} alt="" className="w-3" />
+            <p className="pl-2">(122)</p>
           </div>
-          <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
-          <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
           <div className="flex flex-col gap-4 my-8">
             <p>Select Size</p>
             <div className="flex gap-2">
-              {productData.sizes.map((item,index) => (
-                <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'bg-orange-500 text-white' : ''}`} key={index}>{item}</button>
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "bg-orange-500 text-white" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
               ))}
             </div>
           </div>
 
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
-          <hr className="mt-8 sm:w-4/5"/>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
           <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
-                <p>100% Original product.</p>
-                <p>Cash on delivery is available on this product.</p>
-                <p>Easy return and exchange policy within 7 days.</p>
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
           </div>
         </div>
       </div>
 
       {/* ------- Description & Review Section ---------- */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odio,
+            mollitia, cum quia enim sapiente repellendus nostrum nisi molestias
+            autem aperiam ea natus saepe ipsa! Accusantium nostrum eius amet
+            distinctio?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+            quod eos? Et natus, odio neque deserunt molestias inventore quaerat
+            obcaecati non accusantium distinctio veritatis totam asperiores sunt
+            repellendus eum libero! Quia itaque omnis ratione quo.
+          </p>
+        </div>
+      </div>
+
+      {/* -------- display related products --------- */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : (
     <div className="opacity-0"></div>
