ab8f767e65b04646466ff88a17a72a2a870b8100
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2024-09-29 16:10:59 +0700

    login page

--- e-commerce/src/components/Navbar.jsx
+++ e-commerce/src/components/Navbar.jsx
@@ -6,21 +6,20 @@ import { ShopContext } from "../context/shopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
   const [hiddenSearch, setHiddenSearch] = useState(false);
-  const {setShowSearch, getCartCount} = useContext(ShopContext);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
   const location = useLocation();
 
   useEffect(() => {
     if (location.pathname.includes("collection")) {
-        setHiddenSearch(true);
+      setHiddenSearch(true);
     } else {
-        setHiddenSearch(false);
+      setHiddenSearch(false);
     }
   }, [location]);
 
-
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <Link to={'/'}>
+      <Link to={"/"}>
         <img src={assets.logo} className="w-36" alt="" />
       </Link>
 
@@ -50,13 +49,20 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img onClick={() => setShowSearch(true)} src={assets.search_icon} className={`w-5 cursor-pointer ${hiddenSearch ? '' : 'hidden'}`} alt="" />
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className={`w-5 cursor-pointer ${hiddenSearch ? "" : "hidden"}`}
+          alt=""
+        />
         <div className="group relative">
-          <img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt=""
-          />
+          <Link to={'/login'}>
+            <img
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt=""
+            />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
