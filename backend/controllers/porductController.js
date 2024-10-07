d51f34395bdb89422bf3961e76bbf92253ad29bf
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2024-10-07 17:08:40 +0700

    get product API , delete product API

--- backend/controllers/porductController.js
+++ backend/controllers/porductController.js
@@ -1,57 +1,85 @@
-import { v2 as cloudinary } from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
 import productModel from "../models/porductModel.js";
 
 // function for add product
 const addProduct = async (req, res) => {
-    try {
-        const { name, description, price, category, subcategory, sizes, bestseller } = req.body
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subcategory,
+      sizes,
+      bestseller,
+    } = req.body;
 
-        const image1 = req.files?.image1 && req.files.image1[0] 
-        const image2 = req.files?.image2 && req.files.image2[0] 
-        const image3 = req.files?.image3 && req.files.image3[0] 
-        const image4 = req.files?.image4 && req.files.image4[0] 
+    const image1 = req.files?.image1 && req.files.image1[0];
+    const image2 = req.files?.image2 && req.files.image2[0];
+    const image3 = req.files?.image3 && req.files.image3[0];
+    const image4 = req.files?.image4 && req.files.image4[0];
 
-        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
 
-        let imageUrl = await Promise.all(
-            images.map(async (item) => {
-                let result = await cloudinary.uploader.upload(item.path, {resource_type: 'image'})
-                return result.secure_url;
-            })
-        )
+    let imageUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
 
-        // console.log( name, description, price, category, subCategory, sizes, bestseller );
-        // console.log(imageUrl);
+    // console.log( name, description, price, category, subCategory, sizes, bestseller );
+    // console.log(imageUrl);
 
-        const productData = {
-            name,
-            description,
-            price: Number(price),
-            category,
-            subcategory,
-            sizes: JSON.parse(sizes),
-            bestseller: bestseller === "true" ? true : false,
-            images: imageUrl,
-            date: Date.now()
-        }
+    const productData = {
+      name,
+      description,
+      price: Number(price),
+      category,
+      subcategory,
+      sizes: JSON.parse(sizes),
+      bestseller: bestseller === "true" ? true : false,
+      images: imageUrl,
+      date: Date.now(),
+    };
 
-        console.log(productData);
+    console.log(productData);
 
-        const product = new productModel(productData)
-        await product.save();
+    const product = new productModel(productData);
+    await product.save();
 
-        res.json({success: true, message: "Product Added"})
-    } catch (error) {
-        console.log(error);
-        res.json({success: false, message: error.message})
-    }
+    res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // function for list product
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for removing product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Product Removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for single product info
 const singleProduct = async (req, res) => {};
